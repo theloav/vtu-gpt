4c41d225e5999fb806994176ef68adfd908b083d
theloav <shrivarshan81@gmail.com>
2025-11-29 18:41:08 +0530

    i

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,18 @@ const nextConfig: NextConfig = {
   },
 
   // serverExternalPackages: ['pdf-parse', 'mammoth'] // Removed to allow Next.js to bundle these modules
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push(
+        '@napi-rs/canvas',
+        'canvas',
+        'pdf-parse',
+        'pdfjs-dist',
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
